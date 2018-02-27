ed37a9dc054426c55c4d0bdb144d763b7a8fc111
citation891 <amtrigo891@gmail.com>
2018-02-27 15:44:11 -0500

    Chi-Square Q-Q plotting function

---
+++ R/plotQQ.R
@@ -0,0 +1,34 @@
+#' @title Generate Current Chi-Square Q-Q Plot
+#'
+#' @description This function uses the chiSqrPlot object as an argument and plots the MD vs the associated
+#' Chi-Square distribution value.  The red line plotted is the model for perfect multivariate normality.  The chiSqrPlot
+#' object can be generated via either the `prepareData` or `removeAnomaly` fucntion.
+#' 
+#'
+#' 
+#'
+#' 
+#' @import ggplot2
+#'
+#' 
+#' 
+#'
+#' @param chiSqrPlot Contains observational Chi-square value, MD, time range, and associated block.
+#' 
+#'  
+#'
+#' @return Returns a Chi-Square Q-Q plot.   
+#'
+#' @export
+#' 
+#' 
+plotQQ <- function(chiSqrPlot){
+  
+  ggplot(chiSqrPlot, aes(x = chiSqrVal, y = mahalanobisDistance)) + 
+    geom_point() + 
+    geom_abline(slope = 1, intercept = 0, color = "red") + 
+    xlim(0, max(chiSqrPlot$chiSqrVal)) +
+    ylim(0, max(chiSqrPlot$mahalanobisDistance)) +
+    ggtitle("Chi-Square Q-Q Plot")
+  
+}
